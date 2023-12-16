11e0ba0011acd02f18189dc4106b87ed8f616b82
harshG775 <hgaur491@gmail.com>
2023-12-16 20:27:51 +0530

    reactjs with tailwind initial setup

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import AppLayout from "./layouts/AppLayout.jsx";
+import Home from "./pages/home/Home.jsx";
+
+export default function AppRoutes() {
+	const Routes = createBrowserRouter([
+		{
+			path: "/",
+			element: <AppLayout />,
+			children: [
+				{
+					index: true,
+					element: <Home />,
+				},
+				{
+					path: "/about",
+					element: "<About />",
+				},
+			],
+		},
+	]);
+	return <RouterProvider router={Routes} />;
+}
