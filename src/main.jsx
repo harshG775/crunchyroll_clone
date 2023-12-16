ed005545a7f4ea52c68ac2a662bbcfeda11008d6
harshG775 <hgaur491@gmail.com>
2023-12-16 20:55:14 +0530

    context api with useReducer initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import AppRoutes from "./AppRoutes.jsx";
 import "./global.css";
 
+import ContextProvider_db from "./store/ContextProvider_db.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<AppRoutes />
+		<ContextProvider_db>
+			<AppRoutes />
+		</ContextProvider_db>
 	</React.StrictMode>
 );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,22 @@
+//App.jsx
+import { useContext } from "react";
+import Context_db from "../../store/Context_db";
+import { Actions } from "../../store/Reducer_db";
 
 export default function Home() {
-    return (
-        <div className="bg-red-500">Home</div>
-    )
+	const [state, dispatch] = useContext(Context_db);
+	const handleClick = () => {
+		dispatch({
+			actionType: Actions.TOGGLE_MENU,
+			payload: !state.isOpen,
+		});
+	};
+	return (
+		<div>
+			<h1>App :{`${state.isOpen}`}</h1>
+			<button className="bg-red-500" onClick={handleClick}>
+				toggle
+			</button>
+		</div>
+	);
 }
