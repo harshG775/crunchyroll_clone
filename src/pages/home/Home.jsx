0e219f2e87b3fc26b533ac780fc681fd8267d893
harshG775 <hgaur491@gmail.com>
2023-12-16 23:18:37 +0530

    ////////

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,16 +6,22 @@ import { Actions } from "../../store/Reducer_db";
 import TMDB from "../../helper/TMDB";
 import { useQuery } from "@tanstack/react-query";
 
-
-
 function TrendingAll() {
-	const {data} = useQuery({
+	const { data } = useQuery({
 		queryKey: ["trendingAll"],
-		queryFn: ()=>TMDB.TrendingAll("day"),
+		queryFn: () => TMDB.TrendingAll("day"),
 	});
-	console.log(data)
-	return <div>trendingAll</div>
-
+	console.log(data);
+	return (
+		<ul className="grid gap-4 p-4 grid-cols-[repeat(auto-fit,minmax(200px,1fr))]">
+			{data?.data?.results?.map((item) => (
+				<li key={item.id}>
+					<img className="max-w-64" src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`} alt={item.title} />
+					<h5>{item.title}</h5>
+				</li>
+			))}
+		</ul>
+	);
 }
 export default function Home() {
 	const [state, dispatch] = useContext(Context_db);
