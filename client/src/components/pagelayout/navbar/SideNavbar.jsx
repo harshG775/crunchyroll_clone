5c737828433ffd0ad2dd4f746a9a519c225acf14
Harsh <hgaur491@gmail.com>
2023-12-24 19:21:27 +0530

    installing tailwind-merge

---
+++ client/src/components/pagelayout/navbar/SideNavbar.jsx
@@ -0,0 +1,16 @@
+import { twMerge } from "tailwind-merge";
+import {Icon} from"@iconify/react";
+export default function SideNavbar({setIsSidebarOpen,className}) {
+    return (
+        <nav className={twMerge("flex justify-between bg-slate-800 p-4", className)}>
+            <Icon onClick={() => setIsSidebarOpen(false)} icon="mingcute:menu-fill" />
+            <ul>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/movies">Movies</a></li>
+                <li><a href="/tv">Tv</a></li>
+                <li><a href="/search">Search</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+    )
+}
