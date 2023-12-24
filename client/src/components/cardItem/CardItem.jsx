a1393082e35f0a8fd40b036485e3cb347adaffe6
Harsh <hgaur491@gmail.com>
2023-12-24 12:06:16 +0530

    image poster and backdrop

--- client/src/components/cardItem/CardItem.jsx
+++ client/src/components/cardItem/CardItem.jsx
@@ -1,19 +1,20 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import CardItem_image from "./CardItem_image";
 
 export default function CardItem(props) {
-    const { data } = props
-    return (
-    <Link to={`/overview/${data.media_type}/${data.id}`}>
-        <img
-            className=" rounded-lg overflow-hidden"
-            src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`}
-            alt={data.title}
-        />
-        <p>{data.vote_average}</p>
-        {data.media_type === "tv" ? (
-            <h5>{data.name ? data?.name : data.original_name}</h5>) : (
-            <h5>{data.title? data?.title: data?.original_title}</h5>
-        )}
-    </Link>
-    )
+	const { data } = props;
+	return (
+		<Link to={`/overview/${data?.media_type}/${data?.id}`}>
+			<CardItem_image {...data} />
+			<div className="flex justify-between">
+				<p>{data.vote_average}</p>
+				<div>{data.media_type}</div>
+			</div>
+			{data.media_type === "tv" ? (
+				<h5>{data.name ? data?.name : data.original_name}</h5>
+			) : (
+				<h5>{data.title ? data?.title : data?.original_title}</h5>
+			)}
+		</Link>
+	);
 }
