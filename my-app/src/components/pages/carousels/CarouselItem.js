d3589afdf8720437bfa23a043b1f87fb988aca58
Harsh <hgaur491@gmail.com>
2024-01-03 22:23:03 +0530

    sss

--- my-app/src/components/pages/carousels/CarouselItem.js
+++ my-app/src/components/pages/carousels/CarouselItem.js
@@ -1,7 +1,13 @@
 "use client";
-export default function CarouselItem(props) {
-    const {media_type}=props
+import Link from "next/link";
+export default function CarouselItem(data) {
     return (
-        <div className="text-3xl">{media_type}</div>
+        <Link href={`/overview`}>
+            <div className="flex justify-between">
+                <p>{data.vote_average}</p>
+                <div>{data.media_type}</div>
+            </div>
+            <h2>{data.title}</h2>
+        </Link>
     )
 }
