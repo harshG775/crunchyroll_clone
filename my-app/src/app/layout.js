94b022559cde8165245d18f32d23ca7806f9c522
Harsh <hgaur491@gmail.com>
2024-01-03 21:17:25 +0530

    adding suspense

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,7 +1,12 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { Suspense } from "react";
 import TopNavbar from "@/components/pageLayout/navbar/topNavBar/TopNavbar";
 import SideNavbar from "@/components/pageLayout/navbar/sideNavBar/SideNavbar";
+import Footer from "@/components/pageLayout/footer/Footer";
+import ContextProvider_db from "@/store/ContextProvider_db";
+import PageLoader from "@/components/loaders/pageLoader/PageLoader";
+
+import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -9,16 +14,17 @@ export const metadata = {
 	description: "crunchyroll clone home page",
 };
 
-import ContextProvider_db from "@/store/ContextProvider_db";
 export default function RootLayout({ children }) {
 	return (
 		<ContextProvider_db>
 			<html lang="en">
 				<body className={inter.className}>
-                    <TopNavbar />
-                    <SideNavbar />
-                    <main>{children}</main>
-                    <footer>footer</footer>
+					<TopNavbar />
+					<SideNavbar />
+					<Suspense fallback={<PageLoader />}>
+						<main>{children}</main>
+					</Suspense>
+					<Footer />
 				</body>
 			</html>
 		</ContextProvider_db>
