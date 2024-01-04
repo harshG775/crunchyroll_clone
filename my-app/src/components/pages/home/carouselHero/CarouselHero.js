ad8157413789af7c16e42303707aa8b95b79ec2d
Harsh <hgaur491@gmail.com>
2024-01-04 19:48:00 +0530

    fixing: folder location

--- my-app/src/components/pages/home/carouselHero/CarouselHero.js
+++ my-app/src/components/pages/home/carouselHero/CarouselHero.js
@@ -1,5 +1,5 @@
 import TMDB from "@/helper/TMDB";
-import Carousel from "../../carousels/Carousel";
+import Carousel from "../../../carousels/Carousel";
 export default async function CarouselHero() {
     const resp = await TMDB.TrendingAll("day");
 
