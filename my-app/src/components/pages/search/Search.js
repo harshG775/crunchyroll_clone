60ac14e0c6854e148984c82ae545ead9578d186a
Harsh <hgaur491@gmail.com>
2024-01-07 17:20:18 +0530

    search page form

--- my-app/src/components/pages/search/Search.js
+++ my-app/src/components/pages/search/Search.js
@@ -1,19 +1,23 @@
 "use client"
 import TMDB from "@/helper/TMDB";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { I } from "@/components/iconify/I";
 export default function Search() {
     const [movieGenre, setMovieGenre] = useState([]);
-    const getMovieGenre = async ()=>{
-        const movieGenre = await TMDB.GetGenres("movie",{ language: "en-US" });
+    const getMovieGenre = async (media_type)=>{
+        const movieGenre = await TMDB.GetGenres(media_type,{ language: "en-US" });
         console.log(movieGenre)
         setMovieGenre(movieGenre.data);
     }
+    useEffect(()=>{
+        getMovieGenre("movie");
+    },[])
 	return (
 		<form action="">
-			<ul>
+			<ul className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))]">
                 <li>
                     <label htmlFor="media_type">Media Type</label>
-                    <select onChange={getMovieGenre} name="media_type" id="media_type">
+                    <select onChange={(e)=>getMovieGenre(e.target.value)} name="media_type" id="media_type">
                         <option value="movie">Movie</option>
                         <option value="tv">Tv Show</option>
                     </select>
@@ -25,6 +29,26 @@ export default function Search() {
                             <option key={genre.id} value={genre.id}>{genre.name}</option>
                         ))}
                     </select>
+                </li>
+                <li>
+                    <label htmlFor="language">Language</label>
+                    <select
+                        onChange={(e) => {setSelectedLanguage(e.target.value);}}
+                        name="language"
+                        id="language">
+                        <option value="en-US">English</option>
+                        <option value="hi-IN">Hindi</option>
+                        {/* Add more language options as needed */}
+                    </select>
+                </li>
+                <li className="col-span-2 shadow-lg flex">
+                    <label className="flex-1 flex" htmlFor="query">
+                        <I icon="ic:baseline-search" />
+                        <input className="w-full" type="text" name="query" id="query" />
+                    </label>
+                    <div>
+                        <button type="submit">Search</button>
+                    </div>
                 </li>
 			</ul>
 		</form>
