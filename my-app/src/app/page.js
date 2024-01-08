ca45bbe1aaca36d93ba6d9c1b381cd036bef18d3
Harsh <hgaur491@gmail.com>
2024-01-08 13:40:12 +0530

    folder name

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,7 +1,7 @@
 import Movies from "@/components/pages/home/movies/Movies";
 import CarouselHero from "@/components/pages/home/carouselHero/CarouselHero";
 import TvShow from "@/components/pages/home/tvShows/TvShow";
-import Accordion from "@/components/acord/Aaccordion";
+import Accordion from "@/components/accordion/Accordion";
 export default async function Home() {
 	return (
 		<>
