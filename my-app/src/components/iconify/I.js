33f7100f88c831678699cb6e14d686d626c12345
Harsh <hgaur491@gmail.com>
2024-01-19 20:55:56 +0530

    fixing icon not showing

--- my-app/src/components/iconify/I.js
+++ my-app/src/components/iconify/I.js
@@ -1,11 +1,10 @@
 "use client";
-import useClient from "@/hooks/useClient";
 import { Icon } from "@iconify/react";
 
 export function I(props) {
-    return useClient(()=>{<Icon {...props} />})
+    return <Icon {...props} />
 }
 export function InlineI(props) {
-    return useClient(()=>{<Icon {...props} />})
+    return <Icon {...props} />
 }
 
