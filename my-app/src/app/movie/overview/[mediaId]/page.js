a1837bbc712a1b8b15486fd61afd1dc8034097af
Harsh <hgaur491@gmail.com>
2024-01-21 19:53:09 +0530

    changes ui

--- my-app/src/app/movie/overview/[mediaId]/page.js
+++ my-app/src/app/movie/overview/[mediaId]/page.js
@@ -7,7 +7,7 @@ function Header({data}) {
         <>
             <div className="z-0 absolute inset-[0_0_auto_0] min-h-[30rem] bg-cover bg-[top_left] bg-no-repeat" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${backdrop_path})`}}></div>
             <header className="z-10 backdrop-sepia-0 bg-neutral-900/50">
-                <div className="container mx-auto lg:min-h-[30rem] sm:min-h-[45rem] px-4 py-12 gap-4 grid lg:grid-cols-2">
+                <div className="container mx-auto lg:min-h-[30rem] sm:min-h-[45rem] px-4 py-20 gap-4 grid lg:grid-cols-2">
                     <div className="grid place-content-center lg:place-content-start">
                         <Image
                             width={280}
@@ -16,11 +16,10 @@ function Header({data}) {
                             alt={title}
                         />
                     </div>
-                    <div className="grid place-content-center">
-                        <div className="flex gap-2">
-                            <button>Play</button>
-                            <button>Add</button>
-                            <button>Ratings</button>
+                    <div className="grid items-end justify-center">
+                        <div className="grid grid-cols-2 gap-2">
+                            <button className=" rounded-md px-10 py-2 font-semibold bg-blue-500 hover:bg-blue-800 transition-colors duration-300">Add</button>
+                            <button className=" rounded-md px-10 py-2 font-medium bg-blue-700 hover:bg-blue-900 transition-colors duration-300">Watch</button>
                         </div>
                     </div>
                     <div className="lg:col-span-2 lg:text-start text-center">
