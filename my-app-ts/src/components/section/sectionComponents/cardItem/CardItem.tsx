44e2c820e2b61662a49d41d980427ec85e0202f7
Harsh <hgaur491@gmail.com>
2024-03-15 18:28:34 +0530

    adding items Links href

--- my-app-ts/src/components/section/sectionComponents/cardItem/CardItem.tsx
+++ my-app-ts/src/components/section/sectionComponents/cardItem/CardItem.tsx
@@ -13,12 +13,12 @@ export type Type_cardItemsData = {
     media_type: string;
 }
 export default async function CardItem({data,media_type}:{data:Type_cardItemsData,media_type:string}) {
-    const {name,original_name,title,original_title,vote_average,
+    const {name,original_name,title,original_title,vote_average,id,
         backdrop_path,poster_path}=data
     const titleT =(media_type === "tv") ? ( name ? name : original_name ):( title ? title : original_title );
 	return (
-		<Link className="grid gap-2" href={""}>
-            <CardItem_image backdrop_path={backdrop_path} poster_path={poster_path} title={title}/>
+		<Link className="grid gap-2" href={`/${media_type}/overview/${titleT.replace(/\s+/g, "-")}.=.${id}`}>
+            <CardItem_image backdrop_path={backdrop_path} poster_path={poster_path} title={titleT}/>
             <div className="flex justify-between">
                 <p>{vote_average}</p>
                 <div>{media_type}</div>
