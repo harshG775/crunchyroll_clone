2eba7cc4c4fbe2d13195fcb559d41e977d9e0db2
Harsh <hgaur491@gmail.com>
2024-03-15 10:44:09 +0530

    creating button ui component

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,13 @@
 import Movies from "@/components/pages/home/movies/Movies";
 import CarouselHero from "@/components/pages/home/carouselHero/CarouselHero";
 import TvShow from "@/components/pages/home/tvShows/TvShow";
+import Button from "@/components/ui/Button";
 export default async function Home() {
 	return (
 		<>
+        <Button>
+            Sign Up
+        </Button>
 			<CarouselHero />
 			<Movies />
 			<TvShow />
