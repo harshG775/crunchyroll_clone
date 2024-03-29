a8fe9d933e39210103f374bd17a8750734f91c7f
Harsh <hgaur491@gmail.com>
2024-03-29 17:58:11 +0530

    adding iframe and changing env

--- my-app-ts/src/components/pagesComponents/WatchPlayer/WatchPlayer.tsx
+++ my-app-ts/src/components/pagesComponents/WatchPlayer/WatchPlayer.tsx
@@ -1,6 +1,10 @@
-import React from "react";
-// /movie/watch?id=84958&title=Loki
+// "use client";
+// import React, { useEffect, useState } from "react";
+// const MOVIE = process.env.PUBLIC_MOVIE_PLAYER_URL;
+// const TV = process.env.PUBLIC_TV_PLAYER_URL;
+import {Iframe_tv, Iframe_movie} from "./Iframe";
 export type Type_props = {
+    domain: string;
     media_type: "movie" | "tv";
     title: string;
     id: string;
@@ -9,15 +13,11 @@ export default function WatchPlayer({media_type, title, id }: Type_props) {
     return (
         <section>
             <div>
-                <iframe
-                    className="aspect-video w-full rounded-lg"
-                    src={`https://vidsrc.to/embed/${media_type}/${id}`}
-                    title={title}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                ></iframe>
+                {media_type === "movie" ? (
+                    <Iframe_movie domain="me" title={title} id={id} />
+                ): (
+                    <Iframe_tv domain="me" title={title} id={id} season="1" episode="1"/>
+                )}
                 <h1>{title}</h1>
             </div>
 
