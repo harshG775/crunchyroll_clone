a0c1941ad29c0aec7f04183e32fb44763b13a5f9
Harsh <hgaur491@gmail.com>
2024-04-01 14:46:36 +0530

    fixing env url for iframe

--- my-app-ts/src/components/pagesComponents/playerSection/Iframe.tsx
+++ my-app-ts/src/components/pagesComponents/playerSection/Iframe.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
-
 export type Type_iframe_props = {
     media_src: string;
     title: string;
     id: string;
 };
 export default function Iframe({ media_src, title, id }: Type_iframe_props) {
-    const [isClient, setIsClient] = useState<boolean>(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-    if (!isClient) {
-        return null;
-    }
     return (
         <iframe
             key={id}
